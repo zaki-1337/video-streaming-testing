279c9ba93f2f792e4ebc7a0803a643b3121f6f6a
Naga Saketh V <106236218+SAKETHVUNKA@users.noreply.github.com>
2023-11-20 17:40:24 +0530

    Update views.py

--- vsa_se/video_stream/views.py
+++ vsa_se/video_stream/views.py
@@ -6,6 +6,7 @@ def home(request):
     video_data = [
         {"name": "Video 1", "thumbnail_url": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.VSkK-gvkjC7UgcqMsIxMNwHaHa%26pid%3DApi&f=1&ipt=44e107cf1a8b157c9cc209efad686385f7ea4c87ba88a81e389bab7670ac0f30&ipo=images", "video_url": "gs://se-proj-8c444.appspot.com/WEB-TECH PROJECT PRESENTATION.mp4"},
         {"name": "Video 1", "thumbnail_url": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.VSkK-gvkjC7UgcqMsIxMNwHaHa%26pid%3DApi&f=1&ipt=44e107cf1a8b157c9cc209efad686385f7ea4c87ba88a81e389bab7670ac0f30&ipo=images", "video_url": "https://www.youtube.com/watch?v=7nQsQ0rvYqQ"},
+        {"name": "Breakfast in Heaven", "thumbnail_url": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.VSkK-gvkjC7UgcqMsIxMNwHaHa%26pid%3DApi&f=1&ipt=44e107cf1a8b157c9cc209efad686385f7ea4c87ba88a81e389bab7670ac0f30&ipo=images", "video_url": "gs://se-proj-8c444.appspot.com/Shotgun Willy x Yung Craka - Breakfast in Heaven (Official Music Video) (1) (1).mp4"},
     ]
     return render(request, 'home1.html', {'video_data': video_data})
 
