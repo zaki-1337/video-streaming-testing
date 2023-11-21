66f35742f077b4d1780908590b2a8edbfdf0b0de
Adnan Zaki <zaki.adnan6@gmail.com>
2023-11-21 09:03:50 +0530

    Style fix

--- vsa_se/video_stream/views.py
+++ vsa_se/video_stream/views.py
@@ -5,7 +5,7 @@ from urllib.parse import unquote
 def home(request):
     video_data = [
         {"name": "Interstellar Docking Scene", "thumbnail_url": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EIqYEemIFVVwLIvT_5g72gHaEK%26pid%3DApi&f=1&ipt=4dfc8684ebf0f731aa16df5a2318813146e28e446fafe5a5c197f3bb94fe860a&ipo=images", "video_name": "Interstellar_Docking_Scene"},
-        {"name": "Breakfast in Heaven", "thumbnail_url": "https://i.ytimg.com/vi/cdbyRx11u_8/maxresdefault.jpg", "video_name": "Shotgun Willy x Yung Craka - Breakfast in Heaven (Official Music Video)"},
+        {"name": "Betty Crocker", "thumbnail_url": "https://i.ytimg.com/vi/cdbyRx11u_8/maxresdefault.jpg", "video_name": "Shotgun Willy - Betty Crocker feat. Yung Craka (Official Music Video)"},
     ]
     return render(request, 'home1.html', {'video_data': video_data})
 
